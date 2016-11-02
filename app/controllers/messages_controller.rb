1881edf9b68b0069d43430f2d2518ff6546ecb51
Phat Chiem <thachphat0@gmail.com>
2016-11-02 20:28:45 +0700

    milestone 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,21 @@
 class MessagesController < ApplicationController
 	def index
-		@messages = Message.find_by_room_id params[:room_id]
+		@room = Room.find(params[:room_id])
+	end
+
+	def create
+		@message = Message.new(message_params)
+		if @message.save
+			notice = 'Message created'
+		else
+			notice = 'Message save error'
+		end			
+		redirect_to room_messages_path(room_id: @message.room_id)
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:username, :content, :room_id)
 	end
 
 end
