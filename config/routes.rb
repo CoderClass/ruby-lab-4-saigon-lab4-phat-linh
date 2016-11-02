abf07dd6afc3b4e7a98f9f6cdf2a61090f4b8fd3
Phat Chiem <thachphat0@gmail.com>
2016-11-02 20:41:01 +0700

    milestone 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   	resources :messages
   end
   root 'home#index'
+  get 'logout' => 'home#logout'
+	post 'login' => 'home#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
